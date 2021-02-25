dd2f85cc3eaad3c3df1b89b1163ffe5b852c5ee0
Yaswanth <65965757+Yaswanth-L@users.noreply.github.com>
2021-02-25 19:50:23 +0530

    Update sem7_eee_sgpa.java

--- app/src/main/java/com/example/gpacalculator/sem7_eee_sgpa.java
+++ app/src/main/java/com/example/gpacalculator/sem7_eee_sgpa.java
@@ -78,7 +78,7 @@ public class sem7_eee_sgpa extends AppCompatActivity implements
                 int val6=formatValue(spin7.getSelectedItem().toString())*3;
                 int val7=formatValue(spin8.getSelectedItem().toString())*2;
                 int val8=formatValue(spin9.getSelectedItem().toString())*2;
-                double val=(val1+val2+val3+val4+val5+val6+val7+val8)/23.00;
+                double val=(val1+val2+val3+val4+val5+val6+val7+val8)/22.00;
                 result.setText("Your SGPA is "+Double.toString(val));
             }
         });
